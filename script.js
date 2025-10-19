dd02ea86dd8f9a219e5d542738d5537dabd41213
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-19 13:34:57 +0000

    Move grid building logic into buildGrid function

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ const divLength = 20;
 
 container.style.width = `${(divLength * size) + (size * 2)}px`
 container.style.height = `${(divLength * size) + (size * 2)}px`
-for (let i = 0; i < size * size; i++) {
-    div = document.createElement("div");
-    div.style.width = `${divLength}px`;
-    div.style.height = `${divLength}px`;
-    div.style.backgroundColor = "blue";
-    div.style.border = "1px solid black";
 
-    div.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = "red";
-    });
-    container.appendChild(div);
+function buildGrid() {
+    container.innerHTML = '';
+    for (let i = 0; i < size * size; i++) {
+        div = document.createElement("div");
+        div.style.width = `${divLength}px`;
+        div.style.height = `${divLength}px`;
+        div.style.backgroundColor = "blue";
+        div.style.border = "1px solid black";
 
+        div.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = "red";
+        });
+        container.appendChild(div);
+
+    }
 }
+
+buildGrid(size);
